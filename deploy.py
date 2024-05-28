a050fd563cf7d206a9d97bcf4723729bfae7b2c8
VitaminB16 <artemiy.nosov@gmail.com>
2024-05-28 15:15:54 +0200

    fix: Dependency version

--- deploy.py
+++ deploy.py
@@ -11,6 +11,9 @@ def make_requirements():
 
 def deploy():
     CloudFunctions("phone-location").deploy(path=".", entry_point="entry_point")
+    status = CloudFunctions("phone-location").status()
+    print(f"Status: {status}")
+    return status
 
 
 if __name__ == "__main__":
