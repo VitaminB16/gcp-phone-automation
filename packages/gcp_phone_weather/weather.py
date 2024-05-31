d893cb45097dc1f1eb3b6bfb049e51762095e1c4
VitaminB16 <artemiy.nosov@gmail.com>
2024-05-31 11:37:27 +0200

    feat: Querying weather forecast

--- packages/gcp_phone_weather/weather.py
+++ packages/gcp_phone_weather/weather.py
@@ -4,10 +4,11 @@ load_dotenv()
 
 import os
 import requests
+import pandas as pd
 from gcp_pal import Firestore
 
 
-def query_weather_forecast(latitude, longitude, length=1):
+def query_weather_forecast(latitude, longitude, length=1, parse_output=True):
     """
     Query the weather forecast from the OpenWeatherMap API.
 
@@ -24,12 +25,76 @@ def query_weather_forecast(latitude, longitude, length=1):
     params = {"lat": latitude, "lon": longitude, "appid": api_key}
     response = requests.get(url, params=params)
     output = response.json()
+    if parse_output:
+        output = parse_weather_forecast(output)
     return output
 
 
+def parse_weather_forecast(weather_forecast):
+    """
+    Parse the weather forecast data.
+
+    Args:
+    - weather_forecast (dict): The weather forecast data.
+
+    Returns:
+    - dict: The parsed weather forecast data.
+    """
+    parsed_forecast = {}
+    for forecast in weather_forecast["list"]:
+        date = forecast["dt_txt"]
+        weather = forecast["weather"][0]["description"]
+        temp = forecast["main"]["temp"] - 273.15
+        temp_feels_like = forecast["main"]["feels_like"] - 273.15
+        pressure = forecast["main"]["pressure"]
+        humidity = forecast["main"]["humidity"]
+        wind_speed = forecast["wind"]["speed"]
+        wind_direction = forecast["wind"]["deg"]
+        rain = forecast.get("rain", 0)
+
+        parsed_forecast[date] = {
+            "weather": weather,
+            "temp": temp,
+            "temp_feels_like": temp_feels_like,
+            "pressure": pressure,
+            "humidity": humidity,
+            "wind_speed": wind_speed,
+            "wind_direction": wind_direction,
+            "rain": rain,
+        }
+
+    parsed_forecast = pd.DataFrame(parsed_forecast).T
+
+    return parsed_forecast
+
+
+def obtain_recent_coordinates(device_id=None):
+    """
+    Obtain the most recent coordinates of the device from Firestore.
+
+    Args:
+    - device_id (str): The device ID.
+
+    Returns:
+    - tuple: The latitude and longitude of the device.
+    """
+    if device_id is None:
+        device_id = os.environ["FOLLOWMEE_DEVICE_ID"]
+
+    firestore_time_path = (
+        f"device_locations/last_updated_times/{device_id}/last_updated_time"
+    )
+    last_updated_time = Firestore(firestore_time_path).read()
+    firestore_location_path = (
+        f"device_locations/devices/{device_id}/{last_updated_time}"
+    )
+    last_location = Firestore(firestore_location_path).read()
+    latitude = last_location["Latitude"]
+    longitude = last_location["Longitude"]
+    return latitude, longitude
+
+
 if __name__ == "__main__":
-    latitude = 51.21341
-    longitude = 3.22956
-    query_weather_forecast(latitude, longitude)
-
-
+    latitude, longitude = obtain_recent_coordinates()
+    weather = query_weather_forecast(latitude, longitude)
+    breakpoint()
