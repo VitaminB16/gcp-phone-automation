2c8c0b1f6d9c0faed89e33a2d19b612a25173151
VitaminB16 <artemiy.nosov@gmail.com>
2024-06-02 17:10:17 +0100

    chore: gcp_phone_location module main.py

---
+++ packages/gcp_phone_location/main.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from gcp_pal.utils import log
+
+
+from packages.gcp_phone_location.location import get_current_location, store_location
+
+
+def main():
+    """
+    Stores the current location of all my devices.
+    """
+    location_data = get_current_location()
+    result = store_location(location_data)
+    if result is True:
+        log("Location data stored successfully.")
+    else:
+        raise Exception("Failed to store location data.")
+    return
+
+
+def entry_point(request):
+    main()
+    return {"status": "success"}
+
+
+if __name__ == "__main__":
+    main()
